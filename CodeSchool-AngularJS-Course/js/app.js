a5ba3eeac9916c433355aa4bf17abe600a4039f0
Peter Ivanics <peter.ivanics@reslink.fi>
2016-03-13 20:21:27 +0200

    Added image fields to gem variable in app.js

--- CodeSchool-AngularJS-Course/js/app.js
+++ CodeSchool-AngularJS-Course/js/app.js
@@ -5,34 +5,52 @@
 		this.products = gems;
 	});
 	
+	var imagesPath = "images/";
+	var gemImagesPath = imagesPath + "gems/";
 	var gems = [
 		{
 			name: 'Dodecahedron',
 			price: 2.95,
 			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum libero tincidunt diam accumsan molestie. Sed sit amet nisl ut neque luctus laoreet. Quisque in est faucibus, volutpat ligula congue, vestibulum arcu. In hac habitasse platea dictumst. Nulla dapibus, nisl sed placerat aliquet, magna tellus convallis magna, at tincidunt risus justo quis ligula. Morbi vel dolor eleifend, pharetra elit a, molestie metus. ",
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			image: {
+				full: gemImagesPath + "redgem_full.png",
+				thumbnail: gemImagesPath + "redgem_thumbnail.png"
+			}
 		},
 		{
 			name: 'Pentagonal Gem',
 			price: 4.95,
 			description: "Praesent volutpat eros ipsum. Vivamus sed risus sit amet erat semper maximus eget ac libero. In gravida odio est, vitae molestie ipsum placerat lobortis. Ut rhoncus ullamcorper urna, id ornare arcu tristique a. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce finibus dictum massa ut venenatis. Curabitur dictum posuere augue sed blandit. Etiam sem sapien, auctor vitae sapien quis, finibus aliquam ligula. Donec scelerisque tempor metus in sollicitudin.",
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			image: {
+				full: gemImagesPath + "redgem_full.png",
+				thumbnail: gemImagesPath + "redgem_thumbnail.png"
+			}
 		},
 		{
 			name: 'Shadowgem',
 			price: 2.00,
 			description: "Praesent volutpat eros ipsum. Vivamus sed risus sit amet erat semper maximus eget ac libero. In gravida odio est, vitae molestie ipsum placerat lobortis. Ut rhoncus ullamcorper urna, id ornare arcu tristique a. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce finibus dictum massa ut venenatis. Curabitur dictum posuere augue sed blandit. Etiam sem sapien, auctor vitae sapien quis, finibus aliquam ligula. Donec scelerisque tempor metus in sollicitudin.",
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			image: {
+				full: gemImagesPath + "redgem_full.png",
+				thumbnail: gemImagesPath + "redgem_thumbnail.png"
+			}
 		},
 		{
 			name: 'Tiger\'s Eye',
 			price: 4.45,
 			description: "Praesent volutpat eros ipsum. Vivamus sed risus sit amet erat semper maximus eget ac libero. In gravida odio est, vitae molestie ipsum placerat lobortis. Ut rhoncus ullamcorper urna, id ornare arcu tristique a. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce finibus dictum massa ut venenatis. Curabitur dictum posuere augue sed blandit. Etiam sem sapien, auctor vitae sapien quis, finibus aliquam ligula. Donec scelerisque tempor metus in sollicitudin.",
 			canPurchase: false,
-			soldOut: false
+			soldOut: false,
+			image: {
+				full: gemImagesPath + "redgem_full.png",
+				thumbnail: gemImagesPath + "redgem_thumbnail.png"
+			}
 		}
 	];
 })();
