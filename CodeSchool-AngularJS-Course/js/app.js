6fecb9f343dbeebe20922d17031472f009921d22
Peter Ivanics <peter.ivanics@reslink.fi>
2016-03-13 19:28:32 +0200

    Changed gem to gems array in app.js

--- CodeSchool-AngularJS-Course/js/app.js
+++ CodeSchool-AngularJS-Course/js/app.js
@@ -2,14 +2,23 @@
 	var app = angular.module('store', [ ]);
 	
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.product = gems;
 	});
 	
-	var gem = {
-		name: 'Dodecahedron',
-		price: 2.95,
-		description: ". . .",
-		canPurchase: true,
-		soldOut: false
-	}
+	var gems = [
+		{
+			name: 'Dodecahedron',
+			price: 2.95,
+			description: ". . .",
+			canPurchase: true,
+			soldOut: false
+		},
+		{
+			name: 'Pentagonal Gem',
+			price: 4.95,
+			description: ". . .",
+			canPurchase: true,
+			soldOut: false
+		}
+	];
 })();
