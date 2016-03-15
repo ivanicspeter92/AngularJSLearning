783aa3511da520592c33723b5f5a1ca7a7d884fd
Peter Ivanics <peter.ivanics@reslink.fi>
2016-03-15 22:55:59 +0200

    -

--- CodeSchool-AngularJS-Course/js/app.js
+++ CodeSchool-AngularJS-Course/js/app.js
@@ -83,38 +83,6 @@
 		this.products = gems;
 	});
 
-	app.controller('PanelController', function(){
-		/**
-		 * Indicates which of the tabs in order is selected.
-		 * @type {number}
-		 *
-		 * @author Peter Ivanics
-		 * @date 13.03.2016.
-         */
-		this.tab = 1;
-
-		/**
-		 * Sets the value of the tab variable to the given value.
-		 * @param tab The cardinal order of the tab to be selected.
-		 *
-		 * @author Peter Ivanics
-		 * @date 13.03.2016.
-         */
-		this.selectTab = function(tab) {
-			this.tab = tab;
-		};
-
-		/**
-		 * Tells if the active tab equals to the given number.
-		 *
-		 * @param tab The order of the tab to be checked in the array.
-		 * @returns {boolean}
-         */
-		this.isSelected = function(tab) {
-			return this.tab === tab;
-		};
-	});
-
 	app.controller('GalleryController', function() {
 		this.current = 0;
 
@@ -160,6 +128,45 @@
 		return {
 			restrict: 'E', // specifies the type of the directive - https://docs.angularjs.org/guide/directive
 			templateUrl: 'product-title.html'
+		};
+	});
+
+	app.directive('productPanels', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function(){
+				/**
+				 * Indicates which of the tabs in order is selected.
+				 * @type {number}
+				 *
+				 * @author Peter Ivanics
+				 * @date 13.03.2016.
+				 */
+				this.tab = 1;
+
+				/**
+				 * Sets the value of the tab variable to the given value.
+				 * @param tab The cardinal order of the tab to be selected.
+				 *
+				 * @author Peter Ivanics
+				 * @date 13.03.2016.
+				 */
+				this.selectTab = function(tab) {
+					this.tab = tab;
+				};
+
+				/**
+				 * Tells if the active tab equals to the given number.
+				 *
+				 * @param tab The order of the tab to be checked in the array.
+				 * @returns {boolean}
+				 */
+				this.isSelected = function(tab) {
+					return this.tab === tab;
+				};
+			},
+			controllerAs: 'panel'
 		};
 	});
 })();
